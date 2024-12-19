5ac7c0760a75928295e74f1d150efd6e9a7fdf15
AbdouRoumi <a_roumi@estin.dz>
2024-12-19 20:39:15 +0100

    Additions has been added

--- Parent-PID-Spoofing/PPID-Spoofing.cpp
+++ Parent-PID-Spoofing/PPID-Spoofing.cpp
@@ -2,11 +2,14 @@
 #include <stdio.h>
 
 
-BOOL CreatePPidSpoofedProcess(IN LPCSTR lpProcessName) {
+BOOL CreatePPidSpoofedProcess(IN HANDLE hParentProcess, IN LPCSTR lpProcessName, OUT DWORD* dwProcessId, OUT HANDLE* hProcess, OUT HANDLE* hThread) {
 	HANDLE hProcess;
 	CHAR					lpPath[MAX_PATH * 2];
 	CHAR					WnDr[MAX_PATH];
 	CHAR					CurrentDir[MAX_PATH];
+
+	SIZE_T sThreadSize = NULL;
+	PPROC_THREAD_ATTRIBUTE_LIST		pThreadAttList = NULL;
 
 	STARTUPINFOEXA SiEx = { 0 };
 	PROCESS_INFORMATION		Pi = { 0 };
@@ -29,6 +32,21 @@ BOOL CreatePPidSpoofedProcess(IN LPCSTR lpProcessName) {
 		printf("[!] GetEnvironmentVariableA Failed With Error : %d \n", GetLastError());
 		return EXIT_FAILURE;
 	}
+
+
+	
+	pThreadAttList = (PPROC_THREAD_ATTRIBUTE_LIST)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sThreadSize);
+	if (pThreadAttList == NULL) {
+		printf("[!] HeapAlloc Failed With Error : %d \n",GetLastError());
+		return EXIT_FAILURE;
+	}
+
+	if (!InitializeProcThreadAttributeList(NULL, 1, NULL, &sThreadSize)) {
+		printf("Initializing proc thread attribute list has failed !!\n");
+		return EXIT_FAILURE;
+	}
+
+
 
 
 	if (!CreateProcessA(NULL, lpPath, NULL, NULL, FALSE, EXTENDED_STARTUPINFO_PRESENT, NULL, NULL, &SiEx.StartupInfo, &Pi)) {
